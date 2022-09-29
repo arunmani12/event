fbae4a137c09e4989ec7759c52b0b097ae16fde5
Arunmani <arunmani9787@example.com>
2022-09-29 20:50:24 +0530

    init

---
+++ components/Head.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import React from "react";
+
+const Header = () => {
+  return (
+    <Head>
+      <title>Event</title>
+      <meta name="description" content="our event" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+};
+
+export default Header;
---
+++ components/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from '../styles/Home.module.css'
+
+
+const Main = () => {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>
+        Hello Guys <span style={{ color: "red" }}>&#10084;</span>
+      </h1>
+    </main>
+  );
+};
+
+export default Main;
