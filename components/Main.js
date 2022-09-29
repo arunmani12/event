a7b2cfc98952270d442da3fb2d8afa7b191c395d
Hariharan <nmhariharan2001@gmail.com>
2022-09-29 22:04:09 +0530

    branch check

--- components/Main.js
+++ components/Main.js
@@ -7,7 +7,7 @@ const Main = () => {
       <h1 className={styles.title}>
         Hello Guys <span style={{ color: "red" }}>&#10084;</span>
       </h1>
-      <p>Vanakam da mapla</p>
+      <p>Vanakam da mapla 🙏🙏</p>
     </main>
   );
 };
