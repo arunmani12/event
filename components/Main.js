df1ce66f29425c712f8dc36a204258effd30e2ff
B-Mani-kandan <baskarmanikandan48@gmail.com>
2022-09-30 23:22:19 +0530

    modified

--- components/Main.js
+++ components/Main.js
@@ -1,14 +1,26 @@
 import React from "react";
 import styles from "../styles/Home.module.css";
-
-const Main = () => {
+import Card from "./UI/Card/Card";
+import classes from "./UI/Card/Card.module.css";
+const Main = (props) => {
   return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>
-        Hello Guys <span style={{ color: "red" }}>&#10084;</span>
-      </h1>
-      <p>sollunga bro</p>
-    </main>
+    <div className={classes.wrapper}>
+      <Card
+        title="Card-Title"
+        image="https://media.istockphoto.com/photos/website-design-word-abstract-in-wood-type-picture-id1327278358?b=1&k=20&m=1327278358&s=170667a&w=0&h=9gc99CCh9_cmbCNmEohoWlBdDmpRTfdOKD1hMoghZoQ="
+        description="Web designers plan, create and code internet sites and web pages, many of which combine text with sounds, pictures, graphics and video clips."
+      />
+      <Card
+        title="Card-Title"
+        image="https://media.istockphoto.com/photos/website-design-word-abstract-in-wood-type-picture-id1327278358?b=1&k=20&m=1327278358&s=170667a&w=0&h=9gc99CCh9_cmbCNmEohoWlBdDmpRTfdOKD1hMoghZoQ="
+        description="Web designers plan, create and code internet sites and web pages, many of which combine text with sounds, pictures, graphics and video clips."
+      />
+      <Card
+        title="Card-Title"
+        image="https://media.istockphoto.com/photos/website-design-word-abstract-in-wood-type-picture-id1327278358?b=1&k=20&m=1327278358&s=170667a&w=0&h=9gc99CCh9_cmbCNmEohoWlBdDmpRTfdOKD1hMoghZoQ="
+        description="Web designers plan, create and code internet sites and web pages, many of which combine text with sounds, pictures, graphics and video clips."
+      />
+    </div>
   );
 };
 
